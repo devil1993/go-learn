eef8a03f5dda03fb1cd10436dea28ec4ecea854d
Samanway Ghatak <samanwayghatak@gmail.com>
2025-03-12 08:52:32 +0000

    Second practice.

--- practice-profit-calculaor/profit.go
+++ practice-profit-calculaor/profit.go
@@ -17,14 +17,19 @@ func main() {
 	fmt.Print("Tax Rates: ")
 	fmt.Scan(&taxRate)
 
-	ebt := revenue - expenses
-	profit := float64(ebt) * (1 - float64(taxRate)/100)
-	ratio := float64(ebt) / profit
+	ebt, profit, ratio := calculateMetrics(float64(revenue), float64(expenses), taxRate)
 
 	fmt.Printf("Calculated EBT = %v\n", ebt)
 	fmt.Println("Calculated Profit = ", profit)
 	formattedRatio := fmt.Sprintf("Calculated Ratio = [%.2f]\n", ratio)
 
 	fmt.Println(formattedRatio)
+}
 
+func calculateMetrics(revenue, expenses, taxRate float64) (ebt, profit, ratio float64){
+	ebt = revenue - expenses
+	profit = float64(ebt) * (1 - float64(taxRate)/100)
+	ratio = float64(ebt) / profit
+
+	return ebt, profit, ratio
 }
