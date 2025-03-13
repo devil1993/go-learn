a4f716994d3b197d3a7fd30d8e45dbe731e29790
Samanway Ghatak <samanwayghatak@gmail.com>
2025-03-13 09:26:25 +0000

    Added loop.

--- exercise-if/bank.go
+++ exercise-if/bank.go
@@ -5,34 +5,37 @@ import "fmt"
 func main() {
 	accountBalance := 1000.0
 
-	welcomeMessage()
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
-
-	if choice == 1 {
-		fmt.Println("Your account balance is: ", accountBalance)
-	} else if choice == 2 {
-		fmt.Print("Enter deposit amount: ")
-		var depositAmount float64
-		fmt.Scan(&depositAmount)
-
-		accountBalance += depositAmount
-
-		fmt.Println("Money deposited, your current balance is: ", accountBalance)
-	} else if choice == 3 {
-		fmt.Print("Enter witdraw amount: ")
-		var withdrawAmount float64
-		fmt.Scan(&withdrawAmount)
-
-		accountBalance -= withdrawAmount
-
-		fmt.Println("Money withdrawn, your current balance is: ", accountBalance)
-	} else if choice == 4 {
-		fmt.Println("Goodbye!!")
-		return
+	for{
+		
+		welcomeMessage()
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+	
+		if choice == 1 {
+			fmt.Println("Your account balance is: ", accountBalance)
+		} else if choice == 2 {
+			fmt.Print("Enter deposit amount: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+	
+			accountBalance += depositAmount
+	
+			fmt.Println("Money deposited, your current balance is: ", accountBalance)
+		} else if choice == 3 {
+			fmt.Print("Enter witdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+	
+			accountBalance -= withdrawAmount
+	
+			fmt.Println("Money withdrawn, your current balance is: ", accountBalance)
+		} else if choice == 4 {
+			fmt.Println("Goodbye!!")
+			break
+		}
 	}
-
+	fmt.Println("Thanks for using our bank.")
 }
 
 func welcomeMessage() {
